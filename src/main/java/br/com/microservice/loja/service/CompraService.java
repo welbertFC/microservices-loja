bd3996be92ee6bbcbd7c715c9478997e1c1ca1c4
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2021-07-23 17:56:06 -0300

    create eureka

--- src/main/java/br/com/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.microservice.loja.service;
 
 import br.com.microservice.loja.dto.CompraDto;
 import br.com.microservice.loja.dto.InfoFornecedorDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +10,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     public void realizaCompra(CompraDto compraDto) {
-        var cliente = new RestTemplate();
-        var exchange =  cliente.exchange("http://localhots:8081/info/" + compraDto.getEndereco().getEstado(),
+        //var cliente = new RestTemplate();
+        var exchange =  restTemplate.exchange("http://fornecedor/info/" + compraDto.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDto.class);
         System.out.println(exchange.getBody().getEndereco());
     }
